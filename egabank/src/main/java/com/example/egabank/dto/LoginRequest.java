1173d855216a09da74407ada29d4e101d4b1d190
jamil007-boss <jamiltchabodjo@gmail.com>
2026-01-19 08:46:47 +0000

    Nettoyage total de la branche pour repartir proprement

--- egabank/src/main/java/com/example/egabank/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.egabank.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password; // Changé de motDePasse à password pour correspondre au frontend
-}
--- egabank/src/main/java/com/example/egabank/entity/Mode.java
+++
@@ -1,2 +0,0 @@
-package com.example.egabank.entity;
-public enum Mode { DIRECT, TMONEY, FLOOZ }
